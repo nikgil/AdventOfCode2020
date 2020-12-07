88e9bd828c9018c9cf78a6337aa49a2401ef28aa
nikmanG <ngilevskiy@gmail.com>
2020-12-07 00:37:29 -0500

    day 7

--- src/main/java/io/github/sirnikolai/days/Day7.java
+++ src/main/java/io/github/sirnikolai/days/Day7.java
@@ -2,14 +2,111 @@ package io.github.sirnikolai.days;
 
 import io.github.sirnikolai.AbstractSolution;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class Day7 extends AbstractSolution {
     @Override
     public void Part1(String input) {
+        String[] arr = input.split("\n");
+        Map<String, Bag> bags = new HashMap<>();
 
+        for(String s : arr) {
+            String[] sub = s.split(" contain ");
+            String name = sub[0].substring(0, sub[0].indexOf(" bags"));
+
+            bags.put(name, new Bag(name, sub[1].split(", ")));
+        }
+
+        Set<String> canContain = new HashSet<>();
+
+        for(Bag b : bags.values()) {
+            if(b.map.containsKey("shiny gold")) {
+                canContain.add(b.name);
+            }
+        }
+
+        int prevSize = canContain.size();
+
+        do {
+            prevSize = canContain.size();
+            Set<String> temp = new HashSet<>();
+
+            for(Bag b : bags.values()) {
+                for(String s : canContain) {
+                    if(b.map.containsKey(s)) {
+                        temp.add(b.name);
+                    }
+                }
+            }
+
+            canContain.addAll(temp);
+        } while(canContain.size() != prevSize);
+
+        System.out.println(canContain.size());
     }
 
     @Override
     public void Part2(String input) {
+        String[] arr = input.split("\n");
+        Map<String, Bag> bags = new HashMap<>();
 
+        for(String s : arr) {
+            String[] sub = s.split(" contain ");
+            String name = sub[0].substring(0, sub[0].indexOf(" bags"));
+
+            bags.put(name, new Bag(name, sub[1].split(", ")));
+        }
+
+        System.out.println(getCount(bags, "shiny gold") - 1);
+    }
+
+    private int getCount(Map<String, Bag> bags, String target) {
+        int total = 1;
+        Bag targetBag = bags.get(target);
+
+        for(Map.Entry<String, Integer> e : targetBag.map.entrySet()) {
+            total += e.getValue() * getCount(bags, e.getKey());
+        }
+
+        return total;
+    }
+
+    private static class Bag {
+        String name;
+        Map<String, Integer> map;
+
+        public Bag(String name, String[] contents) {
+            this.name = name;
+            this.map = new HashMap<>();
+
+            if(!contents[0].equals("no other bags.")) {
+                split(contents, map);
+            }
+        }
+
+        private void split(String[] contents, Map<String, Integer> map) {
+            for(String s : contents) {
+                String[] sub = s.split(" ");
+
+                StringBuilder sb = new StringBuilder();
+
+                for(int i = 1; i < sub.length - 1; i++) {
+                    sb.append(sub[i]).append(" ");
+                }
+
+                map.put(sb.toString().trim(), Integer.parseInt(sub[0]));
+            }
+        }
+
+        @Override
+        public String toString() {
+            return "Bag{" +
+                    "name='" + name + '\'' +
+                    ", map=" + map +
+                    '}';
+        }
     }
 }
--- src/test/java/io/github/sirnikolai/days/Day7Test.java
+++ src/test/java/io/github/sirnikolai/days/Day7Test.java
@@ -22,7 +22,7 @@ public class Day7Test {
         System.setOut(new PrintStream(outContent));
         System.setErr(new PrintStream(errContent));
 
-        subject = new Day6();
+        subject = new Day7();
     }
 
     @AfterEach
@@ -34,52 +34,40 @@ public class Day7Test {
     @Test
     public void testPart1() {
         //Given
-        String input = "abc\n" +
-                "\n" +
-                "a\n" +
-                "b\n" +
-                "c\n" +
-                "\n" +
-                "ab\n" +
-                "ac\n" +
-                "\n" +
-                "a\n" +
-                "a\n" +
-                "a\n" +
-                "a\n" +
-                "\n" +
-                "b";
+        String input = "light red bags contain 1 bright white bag, 2 muted yellow bags.\n" +
+                "dark orange bags contain 3 bright white bags, 4 muted yellow bags.\n" +
+                "bright white bags contain 1 shiny gold bag.\n" +
+                "muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.\n" +
+                "shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.\n" +
+                "dark olive bags contain 3 faded blue bags, 4 dotted black bags.\n" +
+                "vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.\n" +
+                "faded blue bags contain no other bags.\n" +
+                "dotted black bags contain no other bags.";
 
         //When
         subject.Part1(input);
 
         //Then
-        assertEquals("11" + System.lineSeparator(), outContent.toString());
+        assertEquals("4" + System.lineSeparator(), outContent.toString());
     }
 
     @Test
     public void testPart2() {
         //Given
-        String input = "abc\n" +
-                "\n" +
-                "a\n" +
-                "b\n" +
-                "c\n" +
-                "\n" +
-                "ab\n" +
-                "ac\n" +
-                "\n" +
-                "a\n" +
-                "a\n" +
-                "a\n" +
-                "a\n" +
-                "\n" +
-                "b";
+        String input = "light red bags contain 1 bright white bag, 2 muted yellow bags.\n" +
+                "dark orange bags contain 3 bright white bags, 4 muted yellow bags.\n" +
+                "bright white bags contain 1 shiny gold bag.\n" +
+                "muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.\n" +
+                "shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.\n" +
+                "dark olive bags contain 3 faded blue bags, 4 dotted black bags.\n" +
+                "vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.\n" +
+                "faded blue bags contain no other bags.\n" +
+                "dotted black bags contain no other bags.";
 
         //When
         subject.Part2(input);
 
         //Then
-        assertEquals("6" + System.lineSeparator(), outContent.toString());
+        assertEquals("32" + System.lineSeparator(), outContent.toString());
     }
 }
